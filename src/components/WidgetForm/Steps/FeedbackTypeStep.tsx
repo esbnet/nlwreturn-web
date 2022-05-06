c4bcd599b7eaedff0b0a3bd83939153beefbe311
Edmilson Soares <esbnet@gmail.com>
2022-05-06 10:48:52 -0300

    final adjustments

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,4 +1,4 @@
-import { feedbacckTypes, FeedbackType } from "..";
+import { feedbackTypes, FeedbackType } from "..";
 import { CloseButton } from "../../CloseButton";
 
 interface FeedbackTypeStepProps {
@@ -15,14 +15,14 @@ export function FeedbackTypeStep(props: FeedbackTypeStepProps) {
       </header>
 
       <div className="flex py-8 gap-2 text-neutral-400">
-        {Object.entries(feedbacckTypes).map(([key, value]) => {
+        {Object.entries(feedbackTypes).map(([key, value]) => {
           return (
             <button
               key={key}
               onClick={() => props.onFeedbackTypeChanged(key as FeedbackType)}
               className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-violet-500 focus:border-violet-500 focus:outline-none"
             >
-              <img src={value.image.src} alt={value.image.alt} />
+              <img src={value.image.source} alt={value.image.alt} />
               <span>{value.title}</span>
             </button>
           );
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,71 +1,68 @@
 import { CloseButton } from "../CloseButton";
-import bugImageUrl from "../../assets/bug.svg";
-import ideaImageUrl from "../../assets/idea.svg";
-import thoughtImageUrl from "../../assets/thought.svg";
+
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
-export const feedbacckTypes = {
+export const feedbackTypes = {
   BUG: {
-    title: "Problema",
+    title: 'Problema',
     image: {
-      src: bugImageUrl,
-      alt: "Imagem de um inseto",
+      source: bugImageUrl,
+      alt: 'Ilustração de um inseto roxo'
     }
   },
   IDEA: {
-    title: "Sugestão",
+    title: 'Ideia',
     image: {
-      src: ideaImageUrl,
-      alt: "Imagem de uma lâmpada",
+      source: ideaImageUrl,
+      alt: 'Lâmpada acesa'
     }
   },
   OTHER: {
-    title: "Outro",
+    title: 'Outro',
     image: {
-      src: thoughtImageUrl,
-      alt: "Imagem de um balão de pensamento",
+      source: thoughtImageUrl,
+      alt: 'Núvem de pensamento'
     }
   },
 }
 
-export type FeedbackType = keyof typeof feedbacckTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
-  const [feedbackSent, setFeedbackSent] = useState(false);
+  const [feedbackSent, setFeedbackSent] = useState(false)
 
   function handleRestartFeedback() {
-    setFeedbackSent(false);
-    setFeedbackType(null);
+    setFeedbackSent(false)
+    setFeedbackType(null)
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
       {feedbackSent ? (
         <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
-      ) : (
+      ): (
         <>
           {!feedbackType ? (
             <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-          ) : (
-            <FeedbackContentStep
-              feedbackType={feedbackType}
-              onFeedbackRestartRequest={handleRestartFeedback}
+          ): (
+            <FeedbackContentStep 
+              feedbackType={feedbackType} 
+              onFeedbackRestartRequested={handleRestartFeedback}
               onFeedbackSent={() => setFeedbackSent(true)}
             />
           )}
-
         </>
       )}
-
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela <a className="underline underline-offset-1" href="https://rocketseat.com.br">Rocketseat</a>
+        Feito com ♥ pela <a href="https://rocketseat.com.br" className="underline underline-offset-2">Rocketseat</a>
       </footer>
     </div>
-
-  );
+  )
 }
